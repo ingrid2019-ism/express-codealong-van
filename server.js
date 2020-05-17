901dfb8e106d3edbe52aa6f43bbd483620cbad1c
Ingrid Hammargren <ingrid.hammargren@gmail.com>
2020-05-17 15:32:22 +0200

    codealong completed

--- server.js
+++ server.js
@@ -1,6 +1,28 @@
 import express from 'express'
 import bodyParser from 'body-parser'
 import cors from 'cors'
+import mongoose from 'mongoose'
+
+const mongoUrl = process.env.MONGO_URL || 'mongodb://localhost/post-codealong'
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.Promise = Promise
+
+const Task = mongoose.model('Task', {
+  text: {
+    type: String,
+    required: true,
+    minlength: 5
+  },
+  complete: {
+    type: Boolean,
+    default: false
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+})
+
 
 // Defines the port the app will run on. Defaults to 8080, but can be 
 // overridden when starting the server. For example:
@@ -16,6 +38,27 @@ app.use(bodyParser.json())
 // Start defining your routes here
 app.get('/', (req, res) => {
   res.send('Hello world')
+})
+
+app.get('/tasks', async (req, res) => {
+  const tasks = await Task.find().sort({ createdAt: 'desc' }).limit(20).exec()
+  res.json(tasks)
+})
+
+app.post('/tasks', async (req, res) => {
+  // retrieve the information sent from the client to out API endpoint
+  const { text, complete } = req.body
+  // use our mongoose model to create the database entry
+  const task = new Task({ text, complete })
+
+  try {
+    // sucess
+    const savedTask = await task.save()
+    res.status(201).json(savedTask)
+  } catch (err) {
+    res.status(400).json({ message: 'Could not save task to the Database', error: err.errors })
+
+  }
 })
 
 // Start the server
